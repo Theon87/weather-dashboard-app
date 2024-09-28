3e062fd60b9ac5277ff317cfe24e8fae5a3fecca
theon87 <theonfrancis17@gmail.com>
2024-09-27 22:52:05 -0400

    defines rout to serve index.html

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,5 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
